afc053fcda86076db9ea2e7fe0b71d1379cf1df4
ari-black-acl <ariannah.black@americancruiselines.com>
2026-01-07 10:51:32 -0500

    add file summary

--- server/src/middlewares/logger.ts
+++ server/src/middlewares/logger.ts
@@ -1,7 +1,7 @@
+/* log each request method and url to console */
 
 import { type Request, type Response, type NextFunction } from 'express';
 
-// logs each request method and url
 export const logger = (req: Request, res: Response, next: NextFunction): void => {
     console.log(`✨ ${req.method} ${req.url}`);
     next();
