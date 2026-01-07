308864f1aa63ae901d00c80edfef5e7959af7657
ari-black-acl <ariannah.black@americancruiselines.com>
2026-01-07 15:23:09 -0500

    add /profile/ route

--- server/src/routes/profile-info-routes.ts
+++ server/src/routes/profile-info-routes.ts
@@ -7,4 +7,8 @@ const router: Router = Router();
 // route to get profile info by steamId
 router.get("/profile/:steamId", getProfileInfo);
 
+// produces error due to missing steamId parameter
+router.get("/profile/", getProfileInfo);
+
+
 export default router;
